6feaf36374784b7fa16bd95cbb56f818db96c3dc
Ivan Biryuk <vanyabiryuk@icloud.com>
2022-06-28 20:18:44 +0300

    Initial commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Vanüsha on 28.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
